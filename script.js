7b449eead6cddcac3d2a482f81a3714397ff633b
s1mplyvitek <borismustanger@gmail.com>
2023-02-17 01:10:50 +0800

    hm6js

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
 let dish = [
     {
         buuza: 'buuza', 
-        price: '70', 
-        expense: '30', 
-        ingridients: ['meat', 'water', 'solt', 'onion',],},
+        price: 70, 
+        expense: 30, 
+        ingridients: ['meat', 'water', 'solt', 'onion',],
+        },
         
     { 
         blackSoup: 'blackSoup', 
-        price: '100', 
-        expense: '40', 
+        price: 100, 
+        expense: 40, 
         ingridients: ['meat', 'water', 'solt',],},
 
     { 
         brtuch: 'brtuch', 
-        price: '150', 
-        expense: '60', 
+        price: 150, 
+        expense: 60, 
         ingridients: ['salad', 'bread', 'meat', 'sauce', 'cheese'],},
 ];
+
+let copy = dish[0];
 console.log(dish);
 //dish.splice(2);
 dish.shift();
-dish.unshift('buuza');
-const price = [70, 100, 150];
-const price2 = [30, 40, 60];
-const sum = price.reduce((total, amount) => total + amount ) - price2.reduce((total, amount) => total + amount);
-console.log(sum)
+dish.unshift(copy);
+console.log(copy);
+console.log(typeof copy);
+console.log(copy.ingridients);
+delete copy.ingridients[1];
+console.log(copy.ingridients);
+
+//console.log(dish);
+//console.log(dish[0].price);
+//console.log(dish[0].expense);
+sum = dish[0].price - dish[0].expense;
+console.log(sum);
+
+//console.log(dish[1].price);
+//console.log(dish[1].expense);
+sum = dish[1].price - dish[1].expense;
+console.log(sum);
+
+//console.log(dish[2].price);
+//console.log(dish[2].expense);
+sum = dish[2].price - dish[2].expense;
+console.log(sum);
+//let num;
+//num = 3 + 3;
+//console.log(num);
+//let a = price;
+//let b = expense;
+//console.log(a - b);
+//copy[0] = 'poza';
+//console.log(copy);
+
+
+//consol.log(dish[0].ingridients);
+
+//const price = [0,1,2]
+
+//console.log(ingridients);
+
+
+//const price = [70, 100, 150];
+//const price2 = [30, 40, 60];
+//const sum = price.reduce((total, amount) => total + amount ) - price2.reduce((total, amount) => total + amount);
+//console.log(sum)
